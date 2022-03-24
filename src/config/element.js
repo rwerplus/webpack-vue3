b837b8946d9eb6bd57e82cac8d51daca88bbcf2b
wgf <localfeng@163.com>
2022-03-24 18:46:32 +0800

    [feat]Delete existing

--- src/config/element.js
+++
@@ -1,5 +0,0 @@
-import 'element-ui/lib/theme-chalk/index.css';
-import Vue from 'vue';
-import Element from 'element-ui';
-// import '../assets/style/theme/index.css'
-Vue.use(Element, { size: 'small', zIndex: 3000 });
